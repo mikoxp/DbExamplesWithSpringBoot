444ec73c4d894c7765240ff0b3e6faf27f858e14
olesmichalxp@tlen.pl <olesmichalxp@tlen.pl>
2020-01-04 19:31:42 +0100

    fix insert data script

--- other_scripts/parser.rb
+++ other_scripts/parser.rb
@@ -6,17 +6,21 @@ def open_parse(url,i)
     b=f.readline
     f.each_line do |line|
         for j in 1..37 do
-            x=line.split(';')
-            index=a[j].index(/[A-Z]/) 
-            diver=a[j][0, index].to_i
-            name=a[j][index,a[j].length]
-            puts diver, name
-            v=(x[j].gsub(/[,]/, '.').to_f/diver)  
-            format = 'INSERT INTO public.cash(day, cash, value) VALUES ( \'%s\', \'%s\', %s);' % [x[0], name,v]
-            puts format
-            i.puts(format)
+            begin
+                x=line.split(';')
+                if x[0].match(/\d{8}/)
+                    index=a[j].index(/[A-Z]/)
+                    diver=a[j][0, index].to_i
+                    name=a[j][index,a[j].length]
+                    v=(x[j].gsub(/[,]/, '.').to_f/diver)
+                    format = 'INSERT INTO public.cash(day, cash, value) VALUES ( \'%s\', \'%s\', %s);' % [x[0], name,v]
+                    i.puts(format)
+                end
+            rescue
+                puts line
+            end
         end
-    end 
+    end
 end
 File.open("data.sql", "w") do |i|
     open_parse('https://www.nbp.pl/kursy/Archiwum/archiwum_tab_a_2013.csv',i)
--- src/main/resources/db/migration/V5__cash_year_view.sql
+++ src/main/resources/db/migration/V5__cash_year_view.sql
@@ -1,4 +1,4 @@
 CREATE OR REPLACE VIEW cash_year AS
 (SELECT date_part('year',day)as year, cash, AVG(value)
 	FROM public.cash
-	GROUP BY cash,date_part('year',day))
+	GROUP BY cash,date_part('year',day));
